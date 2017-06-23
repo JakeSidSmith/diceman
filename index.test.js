085dfa3bd3af2a1022f10ab7f1a333fbfe42e141
Jake 'Sid' Smith <jake@dabapps.com>
2017-06-23 16:50:27 +0100

    Add a test test

---
+++ index.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+'use strict';
+
+describe('A test test', function () {
+
+  it('should run a fake test', function () {
+    expect(true).toBe(true);
+  });
+
+});
